4df471070d93d3dec2c578540b0224ec16e660af
Heron <heronselianus1795@gmail.com>
2018-12-04 21:38:17 +0700

    index dassboard

--- resources/views/menu/index.blade.php
+++ resources/views/menu/index.blade.php
@@ -1,37 +1,54 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layout.tampilan')
 
+@section('title','Kelola Menu Admin!')
+
+@section('content')
+
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+
+
+
+<br><br><br><br><br>
+<div class="countainer">
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-
-    <title>SB Admin 2 - Bootstrap Admin Theme</title>
-
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Menu Admin</title>
     <!-- Bootstrap Core CSS -->
     <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
     <!-- MetisMenu CSS -->
     <link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
-
     <!-- Custom CSS -->
     <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
-
     <!-- Morris Charts CSS -->
     <link href="../vendor/morrisjs/morris.css" rel="stylesheet">
-
     <!-- Custom Fonts -->
     <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+        <!-- Place favicon.ico in the root directory -->
+        <link rel="tcu-regular.png" href="images/atcu-regular.png">
+    <link rel="shortcut icon" type="image/tcu-regular.png" href="images/telkom.png" />
+    <!-- Plugin-CSS -->
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/themify-icons.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/magnific-popup.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/animate.css')}}">
+    <!-- Main-Stylesheets -->
+    <link rel="stylesheet" href="{{ asset('css/normalize.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/style.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/responsive.css')}}">
+    <script src="{{ asset('js/vendor/modernizr-2.8.3.min.js')}}"></script>
 
 </head>
 
@@ -935,3 +952,5 @@
 </body>
 
 </html>
+</div>
+@endsection
