7aaf0fb785f4fa7ed7bc2736d198babd2b069f22
Heron <heronselianus1795@gmail.com>
2019-03-21 09:23:32 +0700

    history

--- resources/views/layout/history.blade.php
+++ resources/views/layout/history.blade.php
@@ -3,35 +3,30 @@
 @section('title','Sejarah Telkom CorpU')
 
 @section('content')
+
 <header class="header-area overlay full-height relative v-center" id="home-page">
-        <div class="absolute anlge-bg"></div>
+        <div class="absolute anlge-bg">
         <div class="container">
             <div class="row v-center">
+            </div><br><br><br><br>
                 <div class="col-xs-12 col-md-7 header-text">
                     <h2>Center of Excellence</h2>
                     <p>Telkom Corporate University (Telkom CorpU) didefinisikan sebagai  wahana strategis
                        yang dirancang untuk mendukung pencapaian misi perusahaan dengan melakukan
                        kegiatan-kegiatan untuk memperkuat dan  memperkokoh kemampuan belajar individu
                        dan organisasi dalam rangka  membentuk Center of Excellence. Dengan adanya
-                       pencanangan program Center  of Excellence dengan Telkom CorpU, maka Telkom
-                       melakukan langkah  breakthroughTelkom Corporate University (Telkom CorpU) didefinisikan sebagai  wahana strategis
+                       pencanangan program Center  of Excellence.
+                       <br><br>
+                       Dengan Telkom CorpUmaka Telkom melakukan langkah  breakthroughTelkom Corporate
+                       University (Telkom CorpU) didefinisikan sebagai  wahana strategis
                        yang dirancang untuk mendukung pencapaian misi perusahaan dengan melakukan
                        kegiatan-kegiatan untuk memperkuat dan  memperkokoh kemampuan belajar individu
                        dan organisasi dalam rangka  membentuk Center of Excellence. Dengan adanya
                        pencanangan program Center  of Excellence dengan Telkom CorpU, maka Telkom
                        melakukan langkah  breakthrough dengan menyempurnakan integrasi sesuai dengan
                        konsep CorpU  yang ideal.
-                       Telkom Corporate University (Telkom CorpU) didefinisikan sebagai  wahana strategis
-                       yang dirancang untuk mendukung pencapaian misi perusahaan dengan melakukan
-                       kegiatan-kegiatan untuk memperkuat dan  memperkokoh kemampuan belajar individu
-                       dan organisasi dalam rangka  membentuk Center of Excellence. dengan menyempurnakan integrasi sesuai dengan
-                       konsep CorpU  yang ideal.
-                       Telkom Corporate University (Telkom CorpU) didefinisikan sebagai  wahana strategis
-                       yang dirancang untuk mendukung pencapaian misi perusahaan dengan melakukan
-                       kegiatan-kegiatan untuk memperkuat dan  memperkokoh kemampuan belajar individu
-                       dan organisasi dalam rangka  membentuk Center of Excellence.
-                       </p>
-                    <a href="#" class="button white">Watch video</a>
+                    </p>
+                <a href="#" class="button white">Watch video</a>
                 </div>
             </div>
         </div>
